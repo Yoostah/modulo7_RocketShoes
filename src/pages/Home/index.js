046d32732c59f207004db4d298be071930d12422
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-17 23:37:23 -0300

    estilização da página HOME

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './style';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-couro-cavalera-marcos-masculino/06/D81-0810-006/D81-0810-006_detalhe2.jpg?ims=326x"
+          alt="Produto"
+        />
+        <strong>Tênis muito Legal</strong>
+        <span>R$99,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-couro-cavalera-marcos-masculino/06/D81-0810-006/D81-0810-006_detalhe2.jpg?ims=326x"
+          alt="Produto"
+        />
+        <strong>Tênis muito Legal</strong>
+        <span>R$99,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-couro-cavalera-marcos-masculino/06/D81-0810-006/D81-0810-006_detalhe2.jpg?ims=326x"
+          alt="Produto"
+        />
+        <strong>Tênis muito Legal</strong>
+        <span>R$99,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-couro-cavalera-marcos-masculino/06/D81-0810-006/D81-0810-006_detalhe2.jpg?ims=326x"
+          alt="Produto"
+        />
+        <strong>Tênis muito Legal</strong>
+        <span>R$99,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-couro-cavalera-marcos-masculino/06/D81-0810-006/D81-0810-006_detalhe2.jpg?ims=326x"
+          alt="Produto"
+        />
+        <strong>Tênis muito Legal</strong>
+        <span>R$99,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-couro-cavalera-marcos-masculino/06/D81-0810-006/D81-0810-006_detalhe2.jpg?ims=326x"
+          alt="Produto"
+        />
+        <strong>Tênis muito Legal</strong>
+        <span>R$99,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
