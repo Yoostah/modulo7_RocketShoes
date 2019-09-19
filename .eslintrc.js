29aa9d9393fab86deef8b18ea8a9bccdb294cf93
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-18 22:51:56 -0300

    configuraçao do reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': [2, 'never'],
+    'no-console': ['error', { allow: ['tron'] }],
   },
 };
