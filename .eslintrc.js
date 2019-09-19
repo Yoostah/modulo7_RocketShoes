4c62e42a05a5981849dcd0b6bde4353afff80864
Thulio Guirelle Horta <Yoostah@users.noreply.github.com>
2019-09-18 23:23:56 -0300

    utilizacao do immer para gerenciar as alteraçoes dos estados

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': [2, 'never'],
     'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
   },
 };
